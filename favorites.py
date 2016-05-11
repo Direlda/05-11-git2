621142921285a4499a16f7ccaafb2f79d29e3b67
Direlda <direlda@gmail.com>
2016-05-11 09:27:26 -0700

    Puppies

--- favorites.py
+++ favorites.py
@@ -1,6 +1,6 @@
 print("Dancing under stars")
 print("Raindrops on roses")
-print("Whiskers on kittens")
+print("Whiskers on puppies")
 print("Bright copper kettles")
 print("Warm woolen mittens")
 print("Wild fluffy foxes")
